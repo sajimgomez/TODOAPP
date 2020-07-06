4223d2b4df543a190728e46e04b97bad64ada43a
Sebastián Adolfo Jiménez Gómez <sajimgomez@gmail.com>
2020-07-06 08:42:28 -0500

    'NE'

--- APPFunc.js
+++ APPFunc.js
@@ -4,3 +4,50 @@ const hello_world = () => {
     
 };
 
+const delete_function = (divElement) => {
+
+    divElement.remove();
+
+};
+
+const add_task = () => {
+
+    const inputField = document.getElementById('inputField');
+
+    const inputFieldContent = inputField.value;
+
+    const divElement = document.createElement('div');
+
+    divElement.setAttribute('class', 'ListItemDiv');
+
+    const inputElement = document.createElement('input');
+
+    inputElement.setAttribute('type', 'checkbox');
+    
+    const labelElement = document.createElement('label');   
+
+    labelElement.textContent = inputFieldContent;
+
+    const buttonElement = document.createElement('button');
+
+    buttonElement.setAttribute('type', 'button');
+
+    buttonElement.setAttribute('class', 'ButtonListItem');
+
+    buttonElement.setAttribute('onclick', 'delete_function(divElement)');
+
+    buttonElement.textContent = 'Delete';
+
+    divElement.appendChild(inputElement);
+
+    divElement.appendChild(labelElement);
+
+    divElement.appendChild(buttonElement);
+
+    const list = document.getElementById('taskList');
+
+    list.appendChild(divElement);
+
+};
+
+
